91bbdf42c77b3214f316b611d41f602a9ae2eaa4
luksko602 <65337371+luksko602@users.noreply.github.com>
2020-10-28 12:07:32 +0100

    Lagt till EpokRestAPI för anrop till epok

---
+++ src/main/java/Epok/EpokRestApi.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Epok;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * REST Web Service
+ *
+ * @author Lukas
+ */
+@Path("epok")
+@RequestScoped
+public class EpokRestApi {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of EpokRestApi
+     */
+    public EpokRestApi() {
+    }
+
+    /**
+     * Retrieves representation of an instance of Epok.EpokRestApi
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public String getXml() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of EpokRestApi
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_XML)
+    public void putXml(String content) {
+    }
+    
+       @Path("{course}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCourseModules(@PathParam("course") String courseCode){
+        System.out.println("Parametern in är: " + courseCode);
+         Course course = new EpokController().getCourse(courseCode);
+         return Response.ok(course).build();
+    }
+}
